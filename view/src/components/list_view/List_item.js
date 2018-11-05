78cc6c1195b88a642e197ab3ea6bc2db1da6a128
duan <hello@duanjun.net>
2018-11-06 00:11:12 +0800

    use CSS grid

--- view/src/components/list_view/List_item.js
+++ view/src/components/list_view/List_item.js
@@ -5,284 +5,258 @@ let unit = style_var.spacing.unit;
 import { ipcRenderer } from "electron";
 
 const Loading = styled.div`
-    background: none;
-    box-sizing: border-box;
-    width: ${props => props.width}px;
-    height: ${unit * 8}px;
-    margin: ${unit * 1.5}px ${unit * 1}px;
-    display: flex;
-    flex-direction: row;
-    justify-content: space-between;
-    align-content: center;
-    .icon {
-        flex-basis: ${unit * 6}px;
-        flex-shrink: 0;
-        height: ${unit * 6}px;
-        border-radius: ${unit / 2}px;
-        background-color: ${style_var.colorBase.greyL1};
-        animation: loading ${style_var.transition.slow} alternate infinite;
+  background: none;
+  box-sizing: border-box;
+  height: ${unit * 8}px;
+  margin: ${unit * 1.5}px ${unit * 1}px;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-content: center;
+  .icon {
+    flex-basis: ${unit * 6}px;
+    flex-shrink: 0;
+    height: ${unit * 6}px;
+    border-radius: ${unit / 2}px;
+    background-color: ${style_var.colorBase.greyL1};
+    animation: loading ${style_var.transition.slow} alternate infinite;
+  }
+  .text {
+    border-radius: ${unit / 2}px;
+    height: ${unit * 6}px;
+    flex-grow: 1;
+    flex-shrink: 1;
+    margin-left: ${unit}px;
+    background-color: ${style_var.colorBase.greyL1};
+    animation: loading ${style_var.transition.slow} alternate infinite;
+  }
+  @keyframes loading {
+    from {
+      background: ${style_var.colorBase.greyL1};
     }
-    .text {
-        border-radius: ${unit / 2}px;
-        height: ${unit * 6}px;
-        flex-grow: 1;
-        flex-shrink: 1;
-        margin-left: ${unit}px;
-        background-color: ${style_var.colorBase.greyL1};
-        animation: loading ${style_var.transition.slow} alternate infinite;
+    to {
+      background: ${style_var.colorBase.greyL2};
     }
-    @keyframes loading {
-        from {
-            background: ${style_var.colorBase.greyL1};
-        }
-        to {
-            background: ${style_var.colorBase.greyL2};
-        }
-    }
+  }
 `;
 const fadein = keyframes`${style_var.animation.fadein}`;
 const Item = styled.div`
-    background: ${style_var.colorBase.white};
-    border-radius: ${unit * 0.5}px;
-    box-sizing: border-box;
-    width: ${props => props.width}px;
-    height: ${unit * 8}px;
-    padding: ${unit}px;
-    padding-right: 0;
-    margin: ${unit * 1.5}px ${unit * 1}px;
+  background: ${style_var.colorBase.white};
+  border-radius: ${unit * 0.5}px;
+  box-sizing: border-box;
+  height: ${unit * 8}px;
+  padding: ${unit}px;
+  padding-right: 0;
+  margin: ${unit * 1.5}px ${unit * 1}px;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-content: center;
+  animation: ${fadein} ${style_var.transition.fast};
+  transition: background ${style_var.transition.fast};
+  &:hover,
+  &.selected {
+    background: ${style_var.colorBase.greyL1};
+    .menu svg {
+      display: block;
+    }
+  }
+  .icon {
+    flex-basis: ${unit * 6}px;
+    flex-shrink: 0;
+    height: ${unit * 6}px;
+    border-radius: ${unit / 2}px;
+    img {
+      width: 100%;
+      height: 100%;
+    }
+  }
+  .invalid-icon {
+    opacity: 0.4;
+    flex-basis: ${unit * 6}px;
+    flex-shrink: 0;
+    height: ${unit * 6}px;
+    border-radius: ${unit / 2}px;
+    background-color: ${style_var.colorBase.white};
+    svg {
+      float: left;
+    }
+    path {
+      fill: ${style_var.colorBase.red};
+    }
+  }
+  .text {
+    opacity: ${props => props.invalid && 0.4};
+    flex-grow: 1;
+    flex-shrink: 1;
     display: flex;
-    flex-direction: row;
-    justify-content: space-between;
-    align-content: center;
-    animation: ${fadein} ${style_var.transition.fast};
-    transition: background ${style_var.transition.fast};
-    &:hover,
-    &.selected {
-        background: ${style_var.colorBase.greyL1};
-        .menu svg {
-            display: block;
-        }
+    flex-direction: column;
+    align-content: stretch;
+    justify-content: center;
+    padding-left: ${unit}px;
+    overflow: hidden;
+    .name {
+      font-size: ${style_var.font.size.m};
+      font-weight: ${style_var.font.weight.normal};
+      text-overflow: ellipsis;
+      white-space: nowrap;
+      overflow: hidden;
     }
-    .icon {
-        flex-basis: ${unit * 6}px;
-        flex-shrink: 0;
-        height: ${unit * 6}px;
-        border-radius: ${unit / 2}px;
-        img {
-            width: 100%;
-            height: 100%;
-        }
+    .path {
+      font-size: ${style_var.font.size.s};
+      font-weight: ${style_var.font.weight.normal};
+      text-overflow: ellipsis;
+      white-space: nowrap;
+      overflow: hidden;
+      direction: ltr;
+      text-align: left;
     }
-    .invalid-icon {
-        opacity: 0.4;
-        flex-basis: ${unit * 6}px;
-        flex-shrink: 0;
-        height: ${unit * 6}px;
-        border-radius: ${unit / 2}px;
-        background-color: ${style_var.colorBase.white};
-        svg {
-            float: left;
-        }
-        path {
-            fill: ${style_var.colorBase.red};
-        }
+  }
+  .menu {
+    flex-grow: 0;
+    flex-shrink: 0;
+    flex-basis: ${unit * 2}px;
+    display: flex;
+    flex-direction: column;
+    align-items: stretch;
+    .click-area {
+      height: 24px;
     }
-    .text {
-        opacity: ${props => props.invalid && 0.4};
-        flex-grow: 1;
-        flex-shrink: 1;
-        display: flex;
-        flex-direction: column;
-        align-content: stretch;
-        justify-content: center;
-        padding-left: ${unit}px;
-        overflow: hidden;
-        .name {
-            font-size: ${style_var.font.size.m};
-            font-weight: ${style_var.font.weight.normal};
-            text-overflow: ellipsis;
-            white-space: nowrap;
-            overflow: hidden;
-        }
-        .path {
-            font-size: ${style_var.font.size.s};
-            font-weight: ${style_var.font.weight.normal};
-            text-overflow: ellipsis;
-            white-space: nowrap;
-            overflow: hidden;
-            direction: ltr;
-            text-align: left;
-        }
+    svg {
+      padding-right: ${unit}px;
+      float: right;
+      display: none;
     }
-    .menu {
-        flex-grow: 0;
-        flex-shrink: 0;
-        flex-basis: ${unit * 2}px;
-        display: flex;
-        flex-direction: column;
-        align-items: stretch;
-        .click-area {
-            height: 24px;
-        }
-        svg {
-            padding-right: ${unit}px;
-            float: right;
-            display: none;
-        }
-    }
+  }
 `;
 
 class List_item extends Component {
-    state = {
-        selected: false
-    };
-    clickMenuHandler = e => {
-        e.preventDefault();
-        ipcRenderer.send("show-list-item-menu", {
-            mouse: {
-                x: e.pageX,
-                y: e.pageY
-            },
-            id: this.props.itemId,
-            path: this.props.path,
-            type: this.props.type,
-            invalid: this.props.invalid
-        });
-    };
-    doubleClickHandler = e => {
-        e.stopPropagation();
-        if (this.props.invalid) return;
-        ipcRenderer.send("item-open", {
-            type: this.props.type,
-            path: this.props.path
-        });
-    };
-    reverse_string = str => {
-        if (!str) return;
-        let splitString = str.split("");
-        let reverseArr = splitString.reverse();
-        return reverseArr.join("");
-    };
-    render() {
-        return this.props.isLoading ? (
-            <Loading width={this.props.width}>
-                <div className="icon" />
-                <div className="text" />
-            </Loading>
+  state = {
+    selected: false
+  };
+  clickMenuHandler = e => {
+    e.preventDefault();
+    ipcRenderer.send("show-list-item-menu", {
+      mouse: {
+        x: e.pageX,
+        y: e.pageY
+      },
+      id: this.props.itemId,
+      path: this.props.path,
+      type: this.props.type,
+      invalid: this.props.invalid
+    });
+  };
+  doubleClickHandler = e => {
+    e.stopPropagation();
+    if (this.props.invalid) return;
+    ipcRenderer.send("item-open", {
+      type: this.props.type,
+      path: this.props.path
+    });
+  };
+  reverse_string = str => {
+    if (!str) return;
+    let splitString = str.split("");
+    let reverseArr = splitString.reverse();
+    return reverseArr.join("");
+  };
+  render() {
+    return this.props.isLoading ? (
+      <Loading>
+        <div className="icon" />
+        <div className="text" />
+      </Loading>
+    ) : (
+      <Item
+        invalid={this.props.invalid}
+        className={this.state.selected && "selected"}
+        onDoubleClick={this.doubleClickHandler}
+        onContextMenu={this.clickMenuHandler}
+        title={this.props.invalid ? "this file is invalid" : null}
+      >
+        {this.props.invalid ? (
+          <div className="invalid-icon">
+            <svg viewBox="0 0 48 48" version="1.1">
+              <g id="Artboard" stroke="none" fill="none" fillRule="evenodd">
+                <g id="ban">
+                  <rect
+                    id="Rectangle"
+                    opacity="0"
+                    x="0"
+                    y="0"
+                    width="48"
+                    height="48"
+                  />
+                  <path
+                    d="M24,2.00000002 C11.85,2.00000002 2.00000002,11.85 2.00000002,24 C2.00000002,36.1505 11.85,46 24,46 C36.1505,46 46,36.1505 46,24 C46,11.85 36.1505,2.00000002 24,2.00000002 Z M6,24 C6,19.75 7.47650002,15.8465 9.93950002,12.768 L35.2325,38.0605 C32.0465781,40.6151582 28.0836689,42.005043 24,42 C14.059,42 6,33.941 6,24 Z M38.0605,35.2325 L12.768,9.93950002 C15.9537684,7.38491376 19.9164958,5.99503086 24,6 C33.941,6 42,14.059 42,24 C42.005043,28.0836689 40.6151582,32.0465781 38.0605,35.2325 Z"
+                    id="Shape"
+                    fillRule="nonzero"
+                  />
+                </g>
+              </g>
+            </svg>
+          </div>
         ) : (
-            <Item
-                invalid={this.props.invalid}
-                width={this.props.width}
-                className={this.state.selected && "selected"}
-                onDoubleClick={this.doubleClickHandler}
-                onContextMenu={this.clickMenuHandler}
-                title={this.props.invalid ? "this file is invalid" : null}
-            >
-                {this.props.invalid ? (
-                    <div className="invalid-icon">
-                        <svg viewBox="0 0 48 48" version="1.1">
-                            <g
-                                id="Artboard"
-                                stroke="none"
-                                fill="none"
-                                fillRule="evenodd"
-                            >
-                                <g id="ban">
-                                    <rect
-                                        id="Rectangle"
-                                        opacity="0"
-                                        x="0"
-                                        y="0"
-                                        width="48"
-                                        height="48"
-                                    />
-                                    <path
-                                        d="M24,2.00000002 C11.85,2.00000002 2.00000002,11.85 2.00000002,24 C2.00000002,36.1505 11.85,46 24,46 C36.1505,46 46,36.1505 46,24 C46,11.85 36.1505,2.00000002 24,2.00000002 Z M6,24 C6,19.75 7.47650002,15.8465 9.93950002,12.768 L35.2325,38.0605 C32.0465781,40.6151582 28.0836689,42.005043 24,42 C14.059,42 6,33.941 6,24 Z M38.0605,35.2325 L12.768,9.93950002 C15.9537684,7.38491376 19.9164958,5.99503086 24,6 C33.941,6 42,14.059 42,24 C42.005043,28.0836689 40.6151582,32.0465781 38.0605,35.2325 Z"
-                                        id="Shape"
-                                        fillRule="nonzero"
-                                    />
-                                </g>
-                            </g>
-                        </svg>
-                    </div>
-                ) : (
-                    <div className="icon">
-                        {this.props.isUrl && (
-                            <img
-                                src={require("../../../../assets/icons/url-icon.svg")}
-                            />
-                        )}
-                        {this.props.isDir && (
-                            <img
-                                src={require("../../../../assets/icons/folder-icon.png")}
-                            />
-                        )}
-                        {this.props.isFile && <img src={this.props.iconPath} />}
-                    </div>
-                )}
-                <div
-                    className="text"
-                    title={this.props.invalid ? null : this.props.path}
+          <div className="icon">
+            {this.props.isUrl && (
+              <img src={require("../../../../assets/icons/url-icon.svg")} />
+            )}
+            {this.props.isDir && (
+              <img src={require("../../../../assets/icons/folder-icon.png")} />
+            )}
+            {this.props.isFile && <img src={this.props.iconPath} />}
+          </div>
+        )}
+        <div
+          className="text"
+          title={this.props.invalid ? null : this.props.path}
+        >
+          <div className="name">{this.props.name}</div>
+          <div className="path">{this.props.path}</div>
+        </div>
+        <div className="menu">
+          <div className="click-area" onClick={this.clickMenuHandler}>
+            <svg width="2px" height="10px" viewBox="0 0 2 10" version="1.1">
+              <g
+                id="Symbols"
+                stroke="none"
+                strokeWidth="1"
+                fill="none"
+                fillRule="evenodd"
+              >
+                <g
+                  id="file/hover"
+                  transform="translate(-238.000000, -8.000000)"
+                  fill="#454545"
+                  fillRule="nonzero"
                 >
-                    <div className="name">{this.props.name}</div>
-                    <div className="path">{this.props.path}</div>
-                </div>
-                <div className="menu">
-                    <div className="click-area" onClick={this.clickMenuHandler}>
-                        <svg
-                            width="2px"
-                            height="10px"
-                            viewBox="0 0 2 10"
-                            version="1.1"
-                        >
-                            <g
-                                id="Symbols"
-                                stroke="none"
-                                strokeWidth="1"
-                                fill="none"
-                                fillRule="evenodd"
-                            >
-                                <g
-                                    id="file/hover"
-                                    transform="translate(-238.000000, -8.000000)"
-                                    fill="#454545"
-                                    fillRule="nonzero"
-                                >
-                                    <g id="Group-2-Copy-3">
-                                        <g
-                                            id="Group-5"
-                                            transform="translate(238.000000, 8.000000)"
-                                        >
-                                            <rect
-                                                id="Rectangle"
-                                                x="0"
-                                                y="0"
-                                                width="2"
-                                                height="2"
-                                            />
-                                            <rect
-                                                id="Rectangle-Copy"
-                                                x="0"
-                                                y="4"
-                                                width="2"
-                                                height="2"
-                                            />
-                                            <rect
-                                                id="Rectangle-Copy-2"
-                                                x="0"
-                                                y="8"
-                                                width="2"
-                                                height="2"
-                                            />
-                                        </g>
-                                    </g>
-                                </g>
-                            </g>
-                        </svg>
-                    </div>
-                </div>
-            </Item>
-        );
-    }
+                  <g id="Group-2-Copy-3">
+                    <g id="Group-5" transform="translate(238.000000, 8.000000)">
+                      <rect id="Rectangle" x="0" y="0" width="2" height="2" />
+                      <rect
+                        id="Rectangle-Copy"
+                        x="0"
+                        y="4"
+                        width="2"
+                        height="2"
+                      />
+                      <rect
+                        id="Rectangle-Copy-2"
+                        x="0"
+                        y="8"
+                        width="2"
+                        height="2"
+                      />
+                    </g>
+                  </g>
+                </g>
+              </g>
+            </svg>
+          </div>
+        </div>
+      </Item>
+    );
+  }
 }
 export default List_item;
--- view/src/components/list_view/List_view.js
+++ view/src/components/list_view/List_view.js
@@ -8,146 +8,104 @@ let style_var = require("../style_var.json");
 import List_item from "./List_item";
 
 const View = styled.div`
-    flex-grow: 1;
-    overflow: auto;
-    padding: 16px;
-    user-select: none;
-    ::-webkit-scrollbar {
-        width: 8px;
+  flex-grow: 1;
+  overflow: auto;
+  padding: 16px;
+  user-select: none;
+  ::-webkit-scrollbar {
+    width: 8px;
+  }
+  ::-webkit-scrollbar-track {
+    background: ${style_var.colorBase.white};
+  }
+  ::-webkit-scrollbar-thumb {
+    border-radius: 999px;
+    background: ${style_var.colorBase.greyL2};
+    &:hover {
+      background: ${style_var.colorBase.black};
     }
-    ::-webkit-scrollbar-track {
-        background: ${style_var.colorBase.white};
-    }
-    ::-webkit-scrollbar-thumb {
-        border-radius: 999px;
-        background: ${style_var.colorBase.greyL2};
-        &:hover {
-            background: ${style_var.colorBase.black};
-        }
-    }
+  }
+  #list-container {
+    display: grid;
+    grid-template-columns: repeat(
+      auto-fill,
+      minmax(${style_var.spacing.unit * 28}px, 1fr)
+    );
+  }
 `;
 
 @inject("viewStore", "dataStore")
 @observer
 class List_view extends React.Component {
-    constructor(props) {
-        super(props);
-        this.listview_ref = React.createRef();
-        this.scroller_ref = React.createRef();
-        this.state = {
-            listitem_width: ""
-        };
+  constructor(props) {
+    super(props);
+    this.listview_ref = React.createRef();
+    this.scroller_ref = React.createRef();
+  }
+  scrollHandler = e => {
+    if (this.scroller_ref.current.scrollTop > 16) {
+      this.props.viewStore.list_scrolled = true;
+    } else {
+      this.props.viewStore.list_scrolled = false;
     }
-    scrollHandler = e => {
-        if (this.scroller_ref.current.scrollTop > 16) {
-            this.props.viewStore.list_scrolled = true;
-        } else {
-            this.props.viewStore.list_scrolled = false;
-        }
-    };
-    get_item_width = () => {
-        // mount时读取，没有减掉scrollbar的宽度
-        let view_width = this.listview_ref.current.clientWidth - 8;
-        let item_margin = 16;
-        let min_width = 248 + item_margin;
-        let dif = (view_width % min_width) / parseInt(view_width / min_width);
-        let result = dif + min_width - item_margin;
-        return parseInt(result);
-    };
-    set_item_width = () => {
-        this.setState({
-            listitem_width: this.get_item_width()
-        });
-    };
-    open_all = () => {
-        let items = this.props.dataStore.getAllCurrentItems;
-        ipcRenderer.send("item-open", items);
-    };
-    componentDidMount() {
-        this.set_item_width();
-        window.addEventListener("resize", this.set_item_width);
-        ipcRenderer.on("item-delete", (event, data) => {
-            this.props.dataStore.deleteItemFromCurrentGroup(data.id);
-        });
-    }
-    componentWillUnmount() {
-        window.removeEventListener("resize", this.set_item_width);
-    }
-    render() {
-        return (
-            <View onScroll={this.scrollHandler} innerRef={this.scroller_ref}>
-                <div
-                    ref={this.listview_ref}
-                    style={{
-                        display: "flex",
-                        flexDirection: "row",
-                        justifyContent: "flex-start",
-                        flexWrap: "wrap"
-                    }}
-                >
-                    {!this.props.dataStore.dataLoaded ? (
-                        <React.Fragment>
-                            <List_item
-                                isLoading
-                                width={this.state.listitem_width}
-                            />
-                            <List_item
-                                isLoading
-                                width={this.state.listitem_width}
-                            />
-                            <List_item
-                                isLoading
-                                width={this.state.listitem_width}
-                            />
-                            <List_item
-                                isLoading
-                                width={this.state.listitem_width}
-                            />
-                            <List_item
-                                isLoading
-                                width={this.state.listitem_width}
-                            />
-                        </React.Fragment>
-                    ) : (
-                        <React.Fragment>
-                            {this.props.dataStore.userData.groups[
-                                this.props.dataStore.getCurrentGroupIndex
-                            ].items.map((item, index) => (
-                                <List_item
-                                    width={this.state.listitem_width}
-                                    key={index}
-                                    itemId={item.id}
-                                    name={item.name}
-                                    path={item.path}
-                                    type={item.type}
-                                    iconPath={item.iconPath}
-                                    invalid={!item.valid}
-                                    isUrl={item.isUrl}
-                                    isFile={item.isFile}
-                                    isDir={item.isDir}
-                                />
-                            ))}
-                            <Cta
-                                count={this.props.dataStore.countValidItems}
-                                boom={this.open_all}
-                            />
-                        </React.Fragment>
-                    )}
-                </div>
-                {this.props.dataStore.dataLoaded && (
-                    <Add_new
-                        isEmpty={
-                            this.props.dataStore.getCurrentGroupItemsLength ===
-                            0
-                        }
-                        browse={() => {
-                            this.props.viewStore.show_add_alert = true;
-                        }}
-                    />
-                )}
-            </View>
-        );
-    }
+  };
+  open_all = () => {
+    let items = this.props.dataStore.getAllCurrentItems;
+    ipcRenderer.send("item-open", items);
+  };
+  componentDidMount() {
+    ipcRenderer.on("item-delete", (event, data) => {
+      this.props.dataStore.deleteItemFromCurrentGroup(data.id);
+    });
+  }
+  render() {
+    return (
+      <View onScroll={this.scrollHandler} innerRef={this.scroller_ref}>
+        <div ref={this.listview_ref} id="list-container">
+          {!this.props.dataStore.dataLoaded ? (
+            <React.Fragment>
+              <List_item isLoading />
+              <List_item isLoading />
+              <List_item isLoading />
+              <List_item isLoading />
+              <List_item isLoading />
+            </React.Fragment>
+          ) : (
+            <React.Fragment>
+              {this.props.dataStore.userData.groups[
+                this.props.dataStore.getCurrentGroupIndex
+              ].items.map((item, index) => (
+                <List_item
+                  key={index}
+                  itemId={item.id}
+                  name={item.name}
+                  path={item.path}
+                  type={item.type}
+                  iconPath={item.iconPath}
+                  invalid={!item.valid}
+                  isUrl={item.isUrl}
+                  isFile={item.isFile}
+                  isDir={item.isDir}
+                />
+              ))}
+              <Cta
+                count={this.props.dataStore.countValidItems}
+                boom={this.open_all}
+              />
+            </React.Fragment>
+          )}
+        </div>
+        {this.props.dataStore.dataLoaded && (
+          <Add_new
+            isEmpty={this.props.dataStore.getCurrentGroupItemsLength === 0}
+            browse={() => {
+              this.props.viewStore.show_add_alert = true;
+            }}
+          />
+        )}
+      </View>
+    );
+  }
 }
 
 export default List_view;
